2cea99e5d3deddca807cfcc33930f8af48748e03
Giacomo Bergami <bergamigiacomo@gmail.com>
2024-03-19 23:52:29 +0000

    Added the licence

--- src/main/java/it/giacomobergami/similarity/IEmbedding.java
+++ src/main/java/it/giacomobergami/similarity/IEmbedding.java
@@ -1,4 +1,24 @@
-package it.giacomobergami.similarity;
+/*
+ * IEmbedding.java
+ * This file is part of simple-ontology-alignment
+ *
+ * Copyright (C) 2024 - Giacomo Bergami
+ *
+ * simple-ontology-alignment is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * simple-ontology-alignment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with simple-ontology-alignment. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ package it.giacomobergami.similarity;
 
 public interface IEmbedding extends AutoCloseable {
 
